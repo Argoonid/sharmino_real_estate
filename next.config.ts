edd9ea46ec0d54530ac2f75b6c14c66dda59c6ce
Georgii <aregntumintime@gmail.com>
2026-08-20 13:31:08 +0300

    chore: optimize config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig: NextConfig = {
-  // Если сборка на GitHub Actions — используем подпапку, на Vercel — корень сайта
+  output: 'export',
+  // basePath активен только при сборке на GitHub Actions
   basePath: isGithubActions ? '/sharmino_real_estate' : '',
   assetPrefix: isGithubActions ? '/sharmino_real_estate/' : '',
   images: {
